6d2f229f37574ad0f8d6ae4ffbd8288e20150a1a
kfrozen <troythm@gmail.com>
2017-10-13 18:12:53 +0800

    Refine index file

--- RNStoryList/index.android.js
+++ RNStoryList/index.android.js
@@ -4,91 +4,12 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  FlatList,
-  Text,
-  ToastAndroid,
-  ActivityIndicator,
-  View
+  AppRegistry
 } from 'react-native';
 import {
-  BaseFlatList
-} from '../RNStoryList/js/widget/BaseFlatList';
-import {
-  ArticlePresenter
-} from '../RNStoryList/js/presenters/ArticlePresenter'
+  ArticleListComp
+} from '../RNStoryList/js/widget/ArticleListComp';
 
-export default class Main extends Component {
-
-  constructor(props){
-    super(props);
-
-    this.state = {
-      isLoading: true,
-      dataSource: []
-    }
-  }
-
-  _onDataLoaded = (error, response) => {
-    this.setState({isLoading: false, dataSource: response});
-  }
-
-  componentDidMount() {
-    let articlePresenter = new ArticlePresenter(this._onDataLoaded);
-
-    articlePresenter.loadArticles();
-  }
-
-  render() {
-    
-    let isShowLoading = this.state.isLoading;
-    let dataSource = this.state.dataSource;
-
-    if(isShowLoading){
-      return (
-        <View style={styles.container}>
-          <ActivityIndicator color='blue' size='large'/>
-        </View>
-      );
-    }
-    else if(dataSource.length == 0){
-      return (
-        <View style={styles.container}>
-          <Text style={styles.errorMsg}>
-            Oops! No video is available now...
-          </Text>
-        </View>
-      );
-    }
-    else{
-      return (
-        <View style={styles.container}>
-          <BaseFlatList
-            data={dataSource}
-          />
-        </View>
-      );
-    }
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 10,
-    justifyContent: 'center'
-  },
-  errorMsg: {
-    flexWrap: 'wrap',
-    fontSize: 20,
-    fontWeight:'bold',
-    textAlign: 'center',
-    alignSelf: 'center',
-    color: '#666666',
-  }
-});
-
-AppRegistry.registerComponent('RNStoryList', () => Main);
+AppRegistry.registerComponent('RNStoryList', () => ArticleListComp);
---
+++ RNStoryList/js/widget/ArticleListComp.js
@@ -0,0 +1,86 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  FlatList,
+  Text,
+  ToastAndroid,
+  ActivityIndicator,
+  View
+} from 'react-native';
+import {
+  BaseFlatList
+} from '../widget/BaseFlatList';
+import {
+  ArticlePresenter
+} from '../presenters/ArticlePresenter'
+
+export class ArticleListComp extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      dataSource: []
+    }
+  }
+
+  _onDataLoaded = (error, response) => {
+    this.setState({isLoading: false, dataSource: response});
+  }
+
+  componentDidMount() {
+    let articlePresenter = new ArticlePresenter(this._onDataLoaded);
+
+    articlePresenter.loadArticles();
+  }
+
+  render() {
+    
+    let isShowLoading = this.state.isLoading;
+    let dataSource = this.state.dataSource;
+
+    if(isShowLoading){
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator color='blue' size='large'/>
+        </View>
+      );
+    }
+    else if(dataSource.length == 0){
+      return (
+        <View style={styles.container}>
+          <Text style={styles.errorMsg}>
+            Oops! No video is available now...
+          </Text>
+        </View>
+      );
+    }
+    else{
+      return (
+        <View style={styles.container}>
+          <BaseFlatList
+            data={dataSource}
+          />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 10,
+    justifyContent: 'center'
+  },
+  errorMsg: {
+    flexWrap: 'wrap',
+    fontSize: 20,
+    fontWeight:'bold',
+    textAlign: 'center',
+    alignSelf: 'center',
+    color: '#666666',
+  }
+});
